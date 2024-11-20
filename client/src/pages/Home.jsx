f407062913fee943587523b6b99df5e9def4eb45
Amit <Amitshah050112003@gmail.com>
2024-11-20 23:40:06 +0530

    create basic server and set client with react and create home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='homepage-container'>
+        <div className="input-container">
+            <input type="email" placeholder ="Enter your email here" name="" id="" /> <br />
+            <input type="text" placeholder ="Enter Room Code" name="" id="" /> <br />
+             <button>Enter Room code</button>
+        </div>
+    </div>
+  )
+}
